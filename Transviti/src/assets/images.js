d70bc692408bb8d328bc9ee5fcbb7078c33774d1
MuhammadJunaid506 <93211293+MuhammadJunaid506@users.noreply.github.com>
2024-02-15 02:52:07 +0500

    removed

--- Transviti/src/assets/images.js
+++
@@ -1,6 +0,0 @@
-import Logo from "./files/images@2x.png";
-import Customer from "./files/customers icon@2x.png";
-import Delete from "./files/delete.png"
-import Mask from "./files/mask.png"
-import Profile from "./files/1 2.png"
-export {Logo, Customer,Delete, Mask, Profile}
